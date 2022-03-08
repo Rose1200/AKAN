462b6803fdb5b404b682cda1274a33f9be0c5ef4
Rose Kamau <rose.kamau@studentmoringaschool.com>
2022-03-08 17:14:27 +0300

    add function

--- css/index.js
+++ css/index.js
@@ -1,4 +1,4 @@
-"Kwadwo","Kwabena","Kwaku","Yaw","Kofi", "Kwame"];
+const malenames =["Kwadwo","Kwabena","Kwaku","Yaw","Kofi", "Kwame" ] 
 const daysOfTheWeek =["Sunday","Monday" ,"Tuesday","Wednesday ","Thursday ","Friday","Saturday" ];
 const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 

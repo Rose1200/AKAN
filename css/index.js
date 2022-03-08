70d1c130c4853e8873f03785077bb42343539a38
Rose Kamau <rose.kamau@studentmoringaschool.com>
2022-03-08 17:08:42 +0300

    add initial javascript file

--- css/index.js
+++ css/index.js
@@ -1,6 +1,4 @@
-const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi", "Kwame"];
-const daysOfTheWeek =["Sunday","Monday" ,"Tuesday","Wednesday ","Thursday ","Friday","Saturday" ];
-const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
 
 let century = "";
 let year = "";

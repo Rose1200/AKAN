fe4bd6de25b47d3bf7914a40551f30decaec4120
Rose Kamau <rose.kamau@studentmoringaschool.com>
2022-03-09 09:35:33 +0300

    add function

--- css/index.js
+++ css/index.js
@@ -18,7 +18,7 @@ const displayAkanName = () => {
         document.getElementById("show-akan-name").innerHTML =`Your Akan name is: ${akanName}`
         document.getElementById("show-akan-name").style.display = 'block'
     } else if( mGender.checked) {
-        fGender.checked = false
+        mGender.checked = false
         getAkanName(maleNames);
         console.log("Akan name "+akanName)
         document.getElementById("show-akan-name").innerHTML =`Your Akan name is: ${akanName}`
